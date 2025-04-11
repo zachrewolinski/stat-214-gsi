c0bd476eea134c9d2cee5bb806b50dff8b40989c
aagarwal1996 <aa3797@berkeley.edu>
2025-04-11 16:22:13 -0700

    updating 3.2 code

--- lab3/code/train_encoder.py
+++ lab3/code/train_encoder.py
@@ -1,7 +1,7 @@
 import torch
 # --- MLM MASKING ---
 # --- MLM MASKING ---
-def mask_tokens(input_ids, vocab_size, mask_token_id, pad_token_id, mlm_prob=0.15):
+def mask_tokens(input_ids, vocab_size, mask_token_id, pad_token_id, mlm_prob=0.15 ):
     '''
     TODO: Implement MLM masking
     Args:
@@ -9,23 +9,9 @@ def mask_tokens(input_ids, vocab_size, mask_token_id, pad_token_id, mlm_prob=0.1
         vocab_size: Vocabulary size
         mask_token_id: Mask token ID
         pad_token_id: Pad token ID
+        mlm_prob: Probability of masking
     '''
-    labels = input_ids.clone()
-    probability_matrix = torch.full(input_ids.shape, mlm_prob)
-    special_tokens_mask = (input_ids == pad_token_id) | (input_ids == mask_token_id)
-    probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
-    masked_indices = torch.bernoulli(probability_matrix).bool()
-
-    indices_replaced = torch.bernoulli(torch.full(input_ids.shape, 0.8)).bool() & masked_indices
-    input_ids[indices_replaced] = mask_token_id
-
-    indices_random = torch.bernoulli(torch.full(input_ids.shape, 0.5)).bool() & masked_indices & ~indices_replaced
-    random_words = torch.randint(vocab_size, input_ids.shape, dtype=torch.long)
-    input_ids[indices_random] = random_words[indices_random]
-
-    labels[~masked_indices] = -100
-    return input_ids, labels
-
+    pass
 
 def train_bert(model, dataloader, tokenizer, epochs=3, lr=5e-4, device='cuda'):
     '''
